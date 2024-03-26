89f3cf972cc001988bab1a534ce170874638e467
Mohammed Maarafi <maarafimohammed@gmail.com>
2024-03-26 06:21:32 +0000

    alo

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -2,8 +2,6 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    if (parent == NULL)
-        return(NULL);
     binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
     if (new_node == NULL)
