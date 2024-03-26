405a7af390312d941e1617c7958f3cb6f6aa81ad
Mohammed Maarafi <maarafimohammed@gmail.com>
2024-03-26 21:17:28 +0000

    alo

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -5,16 +5,16 @@
  *
  * @node:  the node to check.
  *
- * Return: if node is null we return 0 otherwise node is a leaf return 1. 
+ * Return: if node is null we return 0 otherwise node is a leaf return 1.
  */
 
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-    if (node == NULL)
-        return(0);
+	if (node == NULL)
+		return (0);
 
-    if (node->parent != NULL && node->left == NULL && node->right == NULL)
-        return (1);
-    else
-        return (0);
+	if (node->parent != NULL && node->left == NULL && node->right == NULL)
+		return (1);
+	else
+		return (0);
 }
